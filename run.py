44a1d921109dd527c34c6492d8b613cc093a3d8a
toga <togapaulo24@gmail.com>
2017-02-09 23:03:15 +0000

    template new link

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import settings
 from application import create_app
-from flask import render_template
+from flask import render_template, request
 
 app = create_app(settings)
 
@@ -8,8 +8,11 @@ app = create_app(settings)
 def index():
     return render_template('index.html')
 
-@app.route('/links/new')
+@app.route('/links/new', methods=['GET', 'POST'])
 def new_link():
+    if request.method == 'POST':
+        user = request.form['url']
+        return user
     return render_template('links/new.html')
 
 if __name__ == '__main__':
