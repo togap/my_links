679b4c8bb7312416d4f91b1387051f9a8938df30
toga <togapaulo24@gmail.com>
2017-02-16 22:42:42 +0000

    implement foundation

--- application/__init__.py
+++ application/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 def create_app(config):
-    app = Flask(__name__, template_folder='templates')
+    app = Flask(__name__, template_folder='templates', static_folder='static')
     app.config.from_object(config)
     from application.models import db
     db.init_app(app)
