d57270aad7daa263c87cf19d9cc4c96b29ee58e5
toga <togapaulo24@gmail.com>
2017-02-23 00:28:35 +0000

    fixed password save

--- application/models.py
+++ application/models.py
@@ -13,7 +13,7 @@ tags = db.Table('link_tags',
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True)
-    password = db.Column(db.String(10))
+    password = db.Column(db.String(70))
     email = db.Column(db.String(20), unique=True)
     first_name = db.Column(db.String(50))
     last_name = db.Column(db.String(30))
@@ -28,7 +28,7 @@ class User(db.Model):
 
     def __init__(self, username, password, email, first_name, last_name):
         self.username = username
-        self.password = password
+        self.set_password(password)
         self.email = email
         self.first_name = first_name
         self.last_name = last_name
