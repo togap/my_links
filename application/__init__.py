388c65677c1467fddfa21f22ad274f24524e4cc7
toga <togapaulo24@gmail.com>
2017-02-24 23:04:46 +0000

    config flask-login

--- application/__init__.py
+++ application/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask.ext.login import LoginManager
 
 def create_app(config):
     app = Flask(__name__, template_folder='templates', static_folder='static')
     app.config.from_object(config)
     from application.models import db
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
     db.init_app(app)
     return app
