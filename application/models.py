58cb909196358c904cd5c2b09cf3a4da01343dfc
toga <togapaulo24@gmail.com>
2017-03-27 21:23:54 +0000

    fix tablename

--- application/models.py
+++ application/models.py
@@ -5,9 +5,9 @@ from werkzeug.security import (generate_password_hash,
 
 db = SQLAlchemy()
 
-tags = db.Table('link_tags',
-        db.Column('tag_id', db.Integer, db.ForeignKey('tag.id')),
-        db.Column('link_id', db.Integer, db.ForeignKey('link.id'))
+tags = db.Table('myLinks_link_tags',
+        db.Column('tag_id', db.Integer, db.ForeignKey('myLinks_tag.id')),
+        db.Column('link_id', db.Integer, db.ForeignKey('myLinks_link.id'))
     )
 
 class User(db.Model):
@@ -62,7 +62,7 @@ class Link(db.Model):
     author = db.Column(db.Text)
     state = db.Column(db.Boolean)
     favorite = db.Column(db.Boolean)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('myLinks_user.id'))
     user = db.relationship('User',
             backref=db.backref('links', lazy='dynamic'))
     tags = db.relationship('Tag', secondary=tags,
@@ -90,7 +90,7 @@ class Tag(db.Model):
     __tablename__ = 'myLinks_tag'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('myLinks_user.id'))
     user = db.relationship('User',
             backref=db.backref('tags', lazy='dynamic'))
     links = db.relationship('Link', secondary=tags,
